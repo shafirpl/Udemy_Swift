56dc2fbfb32abb87a7c27093e66fc2a23e9c1f47
shafirpl <shafi.rpl@gmail.com>
2020-02-22 19:48:07 -0800

    Started first project

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by shafi on 2020-02-22.
+//  Copyright © 2020 shafirpl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
